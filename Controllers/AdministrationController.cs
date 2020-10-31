50e3ec8b2e1c5e39cde1e2d302d2d13a28198dcd
jaimeandres00 <69541815+jaimeandres00@users.noreply.github.com>
2020-10-31 03:29:37 -0500

    Nueva funcionalidad: es posible editar el nombre de los roles pero no la lista de usuarios.

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Project_COVID_19.Views; 
 using System.Threading.Tasks;
+using Project_COVID_19.Models;
 
 namespace Project_COVID_19.Controllers
 {
@@ -11,9 +12,12 @@ namespace Project_COVID_19.Controllers
 
         private readonly RoleManager<IdentityRole> roleManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<User> userManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [HttpGet]
@@ -53,6 +57,63 @@ namespace Project_COVID_19.Controllers
         {
             var roles = roleManager.Roles;
             return View(roles);
-        }        
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if(role == null)
+            {
+                ViewData["Error Message"] = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            foreach(var user in userManager.Users)
+            {
+                if(await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        } 
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+
+            if(role == null)
+            {
+                ViewData["Error Message"] = $"Role with Id = {model.Id} cannot be found";
+                return View("NotFound");
+            } 
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await roleManager.UpdateAsync(role);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError(String.Empty, error.Description);
+                }
+
+                return View(model);
+            }
+        }               
     }
 }
---
+++ Views/EditRoleModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_COVID_19.Views
+{
+    public class EditRoleModel
+    {
+
+        public EditRoleModel() 
+        {
+            Users = new List<string>();
+        }
+
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Role Name is required")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+
+    }
+}
